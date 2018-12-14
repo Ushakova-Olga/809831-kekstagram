61caa2ed7f22ea61d52533a4f373d33aa8245a0a
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2018-12-14 13:27:54 +0700

    Задание 6. Правки

--- js/popup.js
+++ js/popup.js
@@ -2,28 +2,40 @@
 
 /* Модуль для работы с окнами успеха и ошибки при загрузке изображения */
 (function () {
+
+  var closeSuccess = function () {
+    /* Удалять обработчики для кнопок не стала, т.к. с удалением DOM- элемента
+    так поняла что должны все обработчики удалиться
+    Обработчик для ESC надо удалить, т.к. он вешается на document  и не исчезает
+    при удалении дочернего окна  кнопками */
+    document.removeEventListener('keydown', onSuccessEscPress);
+    document.querySelector('main').removeChild(document.querySelector('main').querySelector('.success'));
+  };
+  var onSuccessEscPress = window.util.createKeydownHandler(closeSuccess, window.util.ESC_KEYCODE);
+
+  var templateSuccess = document.querySelector('#success')
+      .content
+      .querySelector('.success');
+
+  var templateError = document.querySelector('#error')
+      .content
+      .querySelector('.error');
+
+  var closeError = function () {
+    /* Удалять обработчики для кнопок не стала, т.к. с удалением DOM- элемента
+    так поняла что должны все обработчики удалиться
+    Обработчик для ESC надо удалить, т.к. он вешается на document  и не исчезает
+    при удалении дочернего окна  кнопками */
+    document.removeEventListener('keydown', onErrorEscPress);
+    document.querySelector('main').removeChild(document.querySelector('main').querySelector('.error'));
+  };
+  var onErrorEscPress = window.util.createKeydownHandler(closeError, window.util.ESC_KEYCODE);
+
   window.popup = {
     openSuccess: function () {
-      var onSuccessEscPress = function (evt) {
-        window.util.isEscEvent(evt, closeSuccess);
-      };
-      var closeSuccess = function () {
-        /* Удалять обработчики для кнопок не стала, т.к. с удалением DOM- элемента
-        так поняла что должны все обработчики удалиться
-        Обработчик для ESC надо удалить, т.к. он вешается на document  и не исчезает
-        при удалении дочернего окна  кнопками */
-        document.removeEventListener('keydown', onSuccessEscPress);
-        document.querySelector('main').removeChild(node);
-      };
-
-      var templateWnd = document.querySelector('#success')
-            .content
-            .querySelector('.success');
-
-      var node = templateWnd.cloneNode(true);
-      document.querySelector('main').appendChild(node);
-      var openedWnd = document.querySelector('main').querySelector('.success');
-      var openedBtn = openedWnd.querySelector('.success__button');
+      var openedSuccessWnd = templateSuccess.cloneNode(true);
+      document.querySelector('main').appendChild(openedSuccessWnd);
+      var openedBtn = openedSuccessWnd.querySelector('.success__button');
 
       openedBtn.addEventListener('click', function () {
         closeSuccess();
@@ -32,26 +44,9 @@
       document.addEventListener('keydown', onSuccessEscPress);
     },
     openError: function () {
-      var onErrorEscPress = function (evt) {
-        window.util.isEscEvent(evt, closeError);
-      };
-      var closeError = function () {
-        /* Удалять обработчики для кнопок не стала, т.к. с удалением DOM- элемента
-        так поняла что должны все обработчики удалиться
-        Обработчик для ESC надо удалить, т.к. он вешается на document  и не исчезает
-        при удалении дочернего окна  кнопками */
-        document.removeEventListener('keydown', onErrorEscPress);
-        document.querySelector('main').removeChild(node);
-      };
-
-      var templateWnd = document.querySelector('#error')
-            .content
-            .querySelector('.error');
-
-      var node = templateWnd.cloneNode(true);
-      document.querySelector('main').appendChild(node);
-      var openedWnd = document.querySelector('main').querySelector('.error');
-      var openedBtn = openedWnd.querySelectorAll('.error__button');
+      var openedErrorWnd = templateError.cloneNode(true);
+      document.querySelector('main').appendChild(openedErrorWnd);
+      var openedBtn = openedErrorWnd.querySelectorAll('.error__button');
 
       for (var i = 0; i < openedBtn.length; i++) {
         openedBtn[i].addEventListener('click', function () {
--- js/util.js
+++ js/util.js
@@ -2,10 +2,12 @@
 
 /* Служебные функции и константы */
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
+/*  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;*/
 
   window.util = {
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
     MAX_SLIDER_LENGTH: 453,
     MAX_HASHTAGS: 5,
     MAX_LENGTH_HASHTAG: 20,
@@ -19,14 +21,22 @@
       return 0;
     },
     isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
+      if (evt.keyCode === window.util.ESC_KEYCODE) {
         action();
       }
     },
     isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
+      if (evt.keyCode === window.util.ENTER_KEYCODE) {
         action();
       }
+    },
+    createKeydownHandler: function (action, keyCode) {
+      if (keyCode === window.util.ESC_KEYCODE) {
+        return (function () {
+          action();
+        });
+      }
+      return (function () {});
     }
   };
 })();
