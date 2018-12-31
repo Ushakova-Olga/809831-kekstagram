058d3b23f6e67de960a795220a9b52be81fc678c
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2018-12-31 18:14:15 +0700

    Правки

--- js/filters.js
+++ js/filters.js
@@ -7,49 +7,44 @@
   var filterPopularButtonElement = document.querySelector('#filter-popular');
   var filterNewButtonElement = document.querySelector('#filter-new');
   var filterDiscussedButtonElement = document.querySelector('#filter-discussed');
-  var picturesSorting = [];
 
-  var unactivateLastButton = function () {
+  var unsetActiveButton = function () {
     var activeButton = document.querySelector('.' + ACTIVE_CLASS_NAME);
     activeButton.classList.remove(ACTIVE_CLASS_NAME);
   };
 
+  var updatePictures = window.debounce(function (value) {
+    switch (value) {
+      case 'filter-popular':
+        window.pictures.showOriginal();
+        break;
+      case 'filter-new':
+        window.pictures.showRandom();
+        break;
+      case 'filter-discussed':
+        window.pictures.showMostDiscussed();
+        break;
+    }
+  });
+
   /* Эти обработчики навешиваются 1 раз на кнопки фильтров и живут до конца,
   пока пользователь не закрыл окно, поэтому их не удаляю. */
   filterPopularButtonElement.addEventListener('click', function () {
-    unactivateLastButton();
+    unsetActiveButton();
     filterPopularButtonElement.classList.add(ACTIVE_CLASS_NAME);
-    picturesSorting = window.pictures.getInitialArray();
-    updatePictures();
-  });
-
-  var updatePictures = window.debounce(function () {
-    window.pictures.remove();
-    window.pictures.render(picturesSorting);
+    updatePictures('filter-popular');
   });
 
   filterNewButtonElement.addEventListener('click', function () {
-    unactivateLastButton();
+    unsetActiveButton();
     filterNewButtonElement.classList.add(ACTIVE_CLASS_NAME);
-    picturesSorting = window.pictures.getRandom();
-    updatePictures();
+    updatePictures('filter-new');
   });
 
   filterDiscussedButtonElement.addEventListener('click', function () {
-    unactivateLastButton();
+    unsetActiveButton();
     filterDiscussedButtonElement.classList.add(ACTIVE_CLASS_NAME);
-    picturesSorting = [];
-    picturesSorting = window.pictures.getInitialArray();
-    picturesSorting.sort(function (first, second) {
-      if (first.comments < second.comments) {
-        return 1;
-      } else if (first.comments > second.comments) {
-        return -1;
-      }
-      return 0;
-    });
-
-    updatePictures();
+    updatePictures('filter-discussed');
   });
 
   window.filters = {
